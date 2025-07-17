c65f382923ee591085a2a1b9bcd47485e959adf0
Phubadin139 <tlephubadin@gmail.com>
2025-07-17 15:45:33 +0700

    Add files via upload

---
+++ lesson.sql
@@ -0,0 +1,7 @@
+
+SELECT * from Employees
+SELECT EmployeeID, FirstName,LastName from Employees
+SELECT * from Employees WHERE City = 'London'
+SELECT EmployeeID, FirstName,LastName from Employees WHERE City = 'London'
+SELECT Distinct City,Country From Customers
+
