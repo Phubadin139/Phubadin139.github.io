30b7a019d9c8393c2f460e5adbbac5553fb4b006
Phubadin139 <tlephubadin@gmail.com>
2025-08-21 14:21:46 +0700

    Update ex3.sql

--- ex3.sql
+++ ex3.sql
@@ -25,3 +25,25 @@ FROM Orders JOIN Shippers
 ON Shippers.ShipperID=Orders.Shipvia
 WHERE OrderID=10275
 --
+--ต้องการรหัสสินค้า ชื่อสินค้า บริษัทผู้จำหน่าย ประเทศ เอาเฉพาะที่มาจาก UK USA
+SELECT p.ProductID, p.ProductName, s.CompanyName, s.Country
+FROM Products p JOIN Suppliers s ON p.SupplierID = s.SupplierID
+where Country in ('usa', 'uk')
+
+--ต้องการรหัสพนักงาน ชื่อพนักงาน รหัสใบสั่งซื้อที่เก่ี่ยวข้อง เรียงตามรหัสพนักงาน
+SELECT e.EmployeeID, FirstName, o.OrderID
+from  Employees e JOIN Orders o on e.EmployeeID = o.EmployeeID
+ORDER by EmployeeID
+
+--จงแสดงหมายเลขใบสั่งซื้อ, ชื่อบริษัทลูกค้า, สถานที่ส่งของ, และพนักงานผู้ดูแล
+SELECT O.OrderID เลขใบสั่งซื้อ, C.CompanyName ลูกค้า, E.FirstName พนักงาน, O.ShipAddress ส่งไปที่
+FROM Orders O
+join Customers C on O.CustomerID=C.CustomerID
+join Employees E on O.EmployeeID=E.EmployeeID
+
+
+select e.EmployeeID, FirstName , count(*) as [จ านวน order]
+, sum(freight) as [Sum of Freight]
+from Employees e join Orders o on e.EmployeeID = o.EmployeeID
+where year(orderdate) = 1998
+group by e.EmployeeID, FirstName
